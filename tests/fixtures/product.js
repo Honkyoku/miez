798dfacf2d802e4f643f2fa77c497ae0f17696a7
Radu <radu.nedisan@evozon.com>
2016-06-27 19:31:03 +0300

    Removed tests from .gitignore

---
+++ tests/fixtures/product.js
@@ -0,0 +1,12 @@
+var details = require('./details');
+var price = require('./price');
+
+module.exports = {
+  sku: "LG-230-BLK",
+  category: "Computers",
+  title: "LG 230-series Black",
+  slug: "lg-230-series-black",
+  details: details,
+  price: price,
+  active: true
+};
